954aebf9e071c55939807962863e624f0ef30bab
Alexander K <niktonezapomnil@gmail.com>
2020-07-15 15:59:13 +0500

    init

---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+	$('.burger').click(function(e){
+        e.preventDefault();
+        (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+
+        $('.menu-links').toggleClass('active');
+        $('body').on('click', function (e) {
+            var div = $('.menu-links, .burger');
+
+            if (!div.is(e.target) && div.has(e.target).length === 0) {
+                div.removeClass('active');
+            }
+        });
+    });
+
+	$('.anchor[href^="#"]').click(function () {
+        if($(window).innerWidth() <= 1000) {
+           $('.menu-links').removeClass('active'); 
+           $('.burger').removeClass('active');
+        }
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top-50;
+        $('html, body').animate( { scrollTop: destination }, 500, 'swing' );
+        return false;
+    });
+
+    function OpenPopup(popupId) {
+        $('body').removeClass('no-scrolling');
+        $('.popup').removeClass('js-popup-show');
+        popupId = '#' + popupId;
+        $(popupId).addClass('js-popup-show');
+        $('body').addClass('no-scrolling');
+    }
+    $('.pop-op').click(function (e) {
+        e.preventDefault();
+        let data = $(this).data('popup');
+        OpenPopup(data);
+    });
+    function closePopup() {
+        $('.js-close-popup').on('click', function (e) {
+            e.preventDefault();
+            $('.popup').removeClass('js-popup-show');
+            $('body').removeClass('no-scrolling');
+        });
+    }
+    closePopup();
+    function clickClosePopup(popupId) {
+        popupId = '#' + popupId;
+        $(popupId).removeClass('js-popup-show');
+        $('body').removeClass('no-scrolling');
+    }
+
+    function openAccordion() {
+        var wrap = $('.accordion-wrap');
+        var accordion = wrap.find('.accordion-title');
+
+        accordion.on('click', function () {
+          var $this = $(this);
+          var $parent = $(this).parent();
+          var content = $this.next();
+
+          if (content.is(':visible')) {
+            $this.removeClass('active');
+            $parent.removeClass('active');
+            content.slideUp('fast');
+          } else {
+            $this.addClass('active');
+            $parent.addClass('active');
+            content.slideDown('fast');
+          }
+
+        });
+    }
+    openAccordion();
+
+    function parallaxIt(e, target, movement, container) {
+        var $this = container;
+        var relX = e.pageX - $this.offset().left;
+        var relY = e.pageY - $this.offset().top;
+
+        TweenMax.to(target, 1, {
+            x: (relX - $this.width() / 2) / $this.width() * movement,
+            y: (relY - $this.height() / 2) / $this.height() * movement
+        });
+    }
+
+    if($(".main-block").length && window.innerWidth > 1000) {
+        $(".main-block").mousemove(function(e) {
+            var $this = $(this);
+            parallaxIt(e, ".bg", -15, $this);
+        });
+    }
+
+    if($(".main-block").length && window.innerWidth > 1000) {
+        $(".main-block").mousemove(function(e) {
+            var $this = $(this);
+            parallaxIt(e, ".balloon", 20, $this);
+        });
+    }
+
+    if($('.table-wrapper').length) {
+        $('.table-wrapper').scrollbar();
+    }
+});
